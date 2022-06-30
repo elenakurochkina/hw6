73f624f391ed78725caa84028e1047af1e8a1ce6
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-30 14:22:33 +0300

    task1 completed

---
+++ task1/program.cs
@@ -0,0 +1,34 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+больше 0 ввёл пользователь.*/
+
+int InputNumber(string msg)
+{
+    System.Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] Arr(int M)
+{
+    int[] numbers = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        numbers[i] = InputNumber($"enter {i + 1} of {M} numbers ");
+
+    }
+    return numbers;
+}
+
+int CountNum(int[] numbers)
+{
+    int count = 0;
+    foreach (int element in numbers)
+    {
+        if (element > 0) count++;
+    }
+    return count;
+}
+
+int M = InputNumber("M = ");
+int[] myArray = Arr(M);
+System.Console.WriteLine($"Count of positive numbers is {CountNum(myArray)}");
